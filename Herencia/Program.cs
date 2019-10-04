20b60b786ac0b80b8743bd5dc5fef2d81a76aa06
DamianBurboa <damianburboa1997@gmail.com>
2019-10-03 23:06:46 -0700

    Add project files.

---
+++ Herencia/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo =
+                new Triangulo();
+            Console.WriteLine(
+                triangulo.toString());
+
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+
+            double areaTriangulo =
+                triangulo.calcularArea();
+
+            Console.WriteLine(
+                "\nArea de triangulo: " +
+                areaTriangulo);
+
+            Circulo circulo = new Circulo(5.4);
+            Console.WriteLine(
+                "Area del circulo: " +
+                circulo.calcularArea().ToString());
+
+            Figura triangulo2 = new Triangulo();
+
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+
+            Console.WriteLine(
+                "Area del triangulo 2: " +
+                triangulo2.calcularArea().ToString());
+
+            List<Figura> figuras = new List<Figura>();
+
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            foreach (var figura in figuras)
+            {
+                Console.WriteLine("\n Area de figura: " +
+                    figura.calcularArea().ToString());
+            }
+
+            Console.Read();
+        }
+    }
+}
